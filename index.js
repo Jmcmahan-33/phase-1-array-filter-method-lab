916782323d267d3f83acbfbc238bf7d98b1ed410
Jared McMahan <jaredm788@gmail.com>
2022-08-23 14:38:01 -0600

    commit tree 88dd87ee00db33a695459b1aceaaf711ba7589a7

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+// function findMatching(drivers,name) {
+//     let newDriverArray = drivers.filter(name => name.toLowerCase() === drivers.toLowerCase())
+//     return newDriverArray
+// }
+
+function findMatching(drivers, name) {
+    return drivers.filter(match => match.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers,name) {
+    return drivers.filter(match => match.slice(0,2) === name)
+}
+
+// function matchName(obj,name) {
+//     return obj.filter(function(obj){
+//         return obj.name === name
+//     }) 
+    
+// }
+   
+
+function matchName(obj,name) {
+    return obj.filter(match => match.name === name)
+}
